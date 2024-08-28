dd21024b62d04fd1f92b7de7c56721744ef6f698
Zerokyh <koreaguh@naver.com>
2024-08-28 10:38:25 +0900

    사이드바 임시 로그인 설정 추가

--- src/components/molecules/NotLoginButton.tsx
+++ src/components/molecules/NotLoginButton.tsx
@@ -1,8 +1,10 @@
 import { Box } from "@mui/material";
 import LinkedButton from "../atom/LinkedButton";
 import { sizes } from "../../styles/sizes";
+import useThemeStore from "../../store/store";
 
 const NotLoginButton = () => {
+  const { setIsLogin } = useThemeStore();
   return (
     <Box
       sx={{
@@ -13,7 +15,7 @@ const NotLoginButton = () => {
         gap: sizes.gap.medium,
       }}
     >
-      <LinkedButton text="로그인" />
+      <LinkedButton text="로그인" onClick={() => setIsLogin(true)} />
       <LinkedButton text="회원가입" />
     </Box>
   );
