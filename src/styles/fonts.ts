0cc90020101e93dfb19824d5ac53c5a037fdd32a
Zerokyh <koreaguh@naver.com>
2024-08-31 17:15:11 +0900

    8/31 김윤호 - 링크 업데이트

--- src/styles/fonts.ts
+++ src/styles/fonts.ts
@@ -2,7 +2,7 @@ import { createTheme } from "@mui/material";
 
 export const theme = createTheme({
   typography: {
-    fontFamily: '"Pretendard-Regular", Arial, sans-serif',
+    fontFamily: '"RixXladywatermelonR", Arial, sans-serif',
     h1: {
       fontFamily: '"Pretendard-Regular", Arial, sans-serif',
     },
