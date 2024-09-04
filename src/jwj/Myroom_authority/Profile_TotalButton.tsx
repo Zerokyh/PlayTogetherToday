ba6be10be039e4b22c7b3771321f9a8a75defa8e
wooj-hub <wjddnwls0803@naver.com>
2024-09-04 16:29:25 +0900

    9/4 정우진 수정

--- src/jwj/Myroom_authority/Profile_TotalButton.tsx
+++ src/jwj/Myroom_authority/Profile_TotalButton.tsx
@@ -2,7 +2,13 @@ import React from "react";
 import { Button, Box } from "@mui/material";
 import { sizes } from "../../styles/sizes";
 
-const Profile_TotalButton = () => {
+type ProfileTotalButtonProps = {
+  toggleAllProfiles: () => void;
+};
+
+const Profile_TotalButton = ({
+  toggleAllProfiles,
+}: ProfileTotalButtonProps) => {
   return (
     <Box sx={{ display: "flex", gap: 1 }}>
       <Button
@@ -13,6 +19,7 @@ const Profile_TotalButton = () => {
           padding: "4px",
         }}
         variant="outlined"
+        onClick={toggleAllProfiles}
       >
         전체공개/비공개
       </Button>
