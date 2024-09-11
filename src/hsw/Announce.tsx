74c732e2869935ad2986b7146ec4671d188b870f
hsw5710 <pett1475321@gmail.com>
2024-09-11 13:40:11 +0900

    scrollbar

--- src/hsw/Announce.tsx
+++ src/hsw/Announce.tsx
@@ -1,10 +1,46 @@
-import { Box, List, Typography } from "@mui/material";
+import { Box, List, Modal, TextField, Typography } from "@mui/material";
 import Button from "@mui/material/Button";
 import { colors } from "../styles/colors";
 import { sizes } from "../styles/sizes";
 import AnnounceList from "./AnnounceList";
+import { useState } from "react";
+import { height } from "@mui/system";
 
 const Announce = () => {
+  const style = {
+    position: "absolute",
+    top: "50%",
+    left: "50%",
+    transform: "translate(-50%, -50%)",
+    width: "800px",
+    height: "600px",
+    bgcolor: "background.paper",
+    border: "2px solid #000",
+    boxShadow: 24,
+    pt: 2,
+    px: 4,
+    pb: 3,
+    display: "flex",
+    flexDirection: "column",
+    gap: sizes.gap.normal,
+  };
+
+  const [open, setOpen] = useState(false);
+  const [send, setSend] = useState(false);
+
+  const handleSend = () => {
+    setSend(true);
+    alert("게시글 등록이 완료되었습니다.");
+  };
+
+  const handelOpen = () => {
+    setOpen(true);
+  };
+
+  const handleClose = () => {
+    setOpen(false);
+  };
+
   return (
     <Box
       sx={{
@@ -58,6 +94,7 @@ const Announce = () => {
               }}
             >
               <Button
+                onClick={handelOpen}
                 sx={{
                   color: "black",
                   borderRadius: "30px",
@@ -69,6 +106,31 @@ const Announce = () => {
               >
                 새로작성
               </Button>
+              {/* modal */}
+              <Modal
+                open={open}
+                onClose={handleClose}
+                aria-labelledby="child-modal-title"
+                aria-describedby="child-modal-description"
+              >
+                <Box sx={{ ...style }}>
+                  <TextField
+                    label="공지사항의 제목을 입력해주세요"
+                    sx={{ width: "100%" }}
+                  ></TextField>
+                  <TextField
+                    label="내용을 입력해주세요"
+                    InputProps={{ style: { height: "100%" } }}
+                    sx={{
+                      width: "100%",
+                      height: "70%",
+                    }}
+                  ></TextField>
+                  <Button onClick={handleSend}>게시글 등록</Button>
+                  <Button onClick={handleClose}>취소</Button>
+                </Box>
+              </Modal>
+
               <Button
                 sx={{
                   color: "black",
