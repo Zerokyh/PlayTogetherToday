d92584afc3244b4d7258745d57a17ce66f53bf06
Zerokyh <koreaguh@naver.com>
2024-09-14 09:51:57 +0900

    9/14 김윤호 - 채팅페이지 업데이트

--- src/components/organism/Chat/ChatListBox.tsx
+++ src/components/organism/Chat/ChatListBox.tsx
@@ -1,10 +1,12 @@
-import { Box } from "@mui/material";
+import { Box, Typography } from "@mui/material";
 import WidthHalfBox from "../../molecules/WidthHalfBox";
 import ChatListItem from "../../molecules/Chat/ChatListItem";
 import { useEffect, useState } from "react";
 import axios from "axios";
 import LoadingSpiner from "../../atom/Loading/LoadingSpinner";
 import { colors } from "../../../styles/colors";
+import useThemeStore from "../../../store/store";
+import { sizes } from "../../../styles/sizes";
 
 type ChatListsData = {
   chat_id: number;
@@ -18,6 +20,7 @@ type ChatListsData = {
 };
 
 const ChatListBox = () => {
+  const { isTheme } = useThemeStore();
   const [chatList, setChatList] = useState<ChatListsData | null>(null);
   const [isLoading, setIsLoading] = useState(true);
 
@@ -46,6 +49,34 @@ const ChatListBox = () => {
         padding: 0,
       }}
     >
+      <Box
+        component="form"
+        sx={{
+          width: "100%",
+
+          backgroundColor:
+            isTheme == "기본"
+              ? colors.background.primary
+              : colors.sub_background.primary,
+          position: "sticky", // 입력란을 고정
+          bottom: 4,
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <Typography
+          sx={{
+            height: 60,
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            fontSize: sizes.fontSize.medium,
+          }}
+        >
+          {/* {friend_member_nickname} */}
+          뒹굴뒹굴
+        </Typography>
+      </Box>
       <Box sx={{ padding: 3, width: "100%", overflowY: "scroll" }}>
         {isLoading ? (
           <LoadingSpiner
